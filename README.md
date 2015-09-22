1a800082eef5df052ef8cd3516327247f7060b67
Guillaume Badi <gbadi@mailjet.com>
2015-09-22 14:33:51 +0200

    clean

--- README.md
+++ README.md
@@ -1,136 +1,47 @@
 #Simple Mailjet APIv3 wrapper (WIP)
 
-## Usage:
+## Getting Started
 
-    In [1]: from mailjet import Client
+First, make sure you have an API key, and an API secret.
+Once you got them, save them in your environment:
 
-    In [2]: c = Client(auth=(API_KEY, API_SECRET))
+```
+export MJ_APIKEY_PUBLIC='your api key'
+export MJ_APIKEY_PRIVATE='your api secret'
+```
 
-    In [3]: c.
-    c.AXTesting                   c.CampaignGraphStats          c.Contactslist                c.MessageInformation          c.OpenStats
-    c.Aggregategraphstatistics    c.CampaignOverview            c.DomainStats                 c.MessageSentStats            c.ParseRoute
-    c.ApiKey                      c.CampaignStats               c.EventCallbackURL            c.MessageState                c.Preferences
-    c.ApiKeyAccess                c.ClickStats                  c.GEOStats                    c.MessageStats                c.Sender
-    c.ApiKeyTotals                c.Contact                     c.GraphStats                  c.MetaSender                  c.SenderStats
-    c.ApiToken                    c.ContactData                 c.ListRecipient               c.Metadata                    c.ToplinkClicked
-    c.Batchjob                    c.ContactFilter               c.ListRecipientstatistics     c.MyProfile                   c.Trigger
-    c.BounceStats                 c.ContactHistoryData          c.ListStats                   c.Newsletter                  c.User
-    c.CSVImport                   c.ContactMetadata             c.ManyContacts                c.NewsletterTemplate          c.UseragentStats
-    c.Campaign                    c.ContactStats                c.Message                     c.NewsletterTemplateCategory  c.Widget
-    c.CampaignAggregate           c.ContactsListSignup          c.MessageHistory              c.OpenInformation             c.WidgetCustomValue
+``` python
+# import the mailjet wrapper
+from mailjet import Client
+import os
 
-    In [4]: c.Campaign.get_many
-    Out[4]: {u'Count': 0, u'Data': [], u'Total': 0}
+# Get your environment Mailjet keys
+API_KEY = os.environ['MJ_APIKEY_PUBLIC']
+API_SECRET = os.environ['MJ_APIKEY_PRIVATE']
 
-    In [5: c.Campaign.get(1)
-    Out[5:
-    {u'Count': 1,
-     u'Data': [{u'CampaignType': 2,
-       u'ClickTracked': 3,
-       u'CreatedAt': u'2014-11-27T14:38:11Z',
-       u'CustomValue': u'mj.nl=1',
-       u'FirstMessageID': 16888499051509351,
-       u'FromEmail': u'tomas.berger@imper.cz',
-       u'FromID': 2,
-       u'FromName': u'Tom',
-       u'HasHtmlCount': 3,
-       u'HasTxtCount': 3,
-       u'ID': 1,
-       u'IsDeleted': False,
-       u'IsStarred': False,
-       u'ListID': 1,
-       u'NewsLetterID': 1,
-       u'OpenTracked': 3,
-       u'SendEndAt': u'2014-11-27T14:38:11Z',
-       u'SendStartAt': u'2014-11-27T14:38:11Z',
-       u'SpamassScore': 1.464,
-       u'Status': 0,
-       u'Subject': u'Test',
-       u'UnsubscribeTrackedCount': 0}],
-     u'Total': 1}
+mailjet = Client(auth=(API_KEY, API_SECRET))
 
+```
 
-    In [6]: c.Campaign.get(2)
+## Make a `GET` request:
+``` python
+# get every contacts
+result = mailjet.contact.get()
+```
 
-    DoesNotExistError                         Traceback (most recent call last)
-    <ipython-input-8-4a064dbddd21> in <module>()
+## `GET` request with filters:
+``` python
+# get the 2 first contacts
+result = mailjet.contact.get(filters={'limit': 2})
+```
+## `POST` request
+``` python
+# Register a new sender email address
+result = mailjet.sender.post(data={'email': 'test@mailjet.com'})
+```
 
-    ...
-
-    DoesNotExistError: {'info': u'', 'request': <PreparedRequest [GET]>, 'message': u'Object not found', 'response_parsed': {u'StatusCode': 404, u'ErrorInfo': u'', u'ErrorMessage
-    ': u'Object not found'}, 'response': <Response [404]>}
-
-
-    In [7]: c.Campaign.
-    c.Campaign.create    c.Campaign.delete    c.Campaign.get       c.Campaign.get_many  c.Campaign.new       c.Campaign.update
-
-    In [8]: help(c.Campaign)
-    Help on Campaign in module mailjet.client object:
-
-    class Campaign(Endpoint)
-     |  Method resolution order:
-     |      Campaign
-     |      Endpoint
-     |      __builtin__.object
-     |
-     |  Methods inherited from Endpoint:
-     |
-     |  __init__(self, url, doc, auth)
-     |
-     |  create(self, data, **kwargs)
-     |
-     |  delete(self, res_id, **kwargs)
-     |
-     |  get(self, res_id, **kwargs)
-     |
-     |  get_many(self, **kwargs)
-     |
-     |  new = create(self, data, **kwargs)
-     |
-     |  update(self, res_id, data, **kwargs)
-     |
-     |  ----------------------------------------------------------------------
-     |  Data descriptors inherited from Endpoint:
-     |
-     |  __dict__
-     |      dictionary for instance variables (if defined)
-     |
-     |  __weakref__
-     |      list of weak references to the object (if defined)
-
-
-    In [9]: c.Campaign._doc
-    Out[9]: 'http://dev.mailjet.com/email-api/v3/campaign'
-
-    In [10]: c.Campaign._url
-    Out[10]: 'https://api.mailjet.com/v3/REST/campaign'
-
-    In [11]: data={
-            'AllowedAccess': 'campaigns,contacts,reports,stats,preferences,property,contact_filter,account,pricing',
-            'TokenType': 'iframe',
-            'ValidFor': 500000,
-            'IsActive': True}
-
-
-    In [12]: c.ApiToken.create(data=data)
-    Out[12]:
-    {u'Count': 1,
-     u'Data': [{u'APIKeyID': 111111,
-       u'AllowedAccess': u'campaigns,contacts,reports,stats,preferences,property,contact_filter,account,pricing',
-       u'CatchedIp': u'127.0.0.1',
-       u'CreatedAt': u'2014-11-29T15:30:05Z',
-       u'FirstUsedAt': u'',
-       u'ID': 1536460,
-       u'IsActive': True,
-       u'Lang': u'',
-       u'LastUsedAt': u'',
-       u'SentData': u'',
-       u'Timezone': u'',
-       u'Token': u'xxxxxxxxxxxxxxxxxxxxxxxF4FE61C3BBC3146F9FA17A760F171250C10502C212EF00DE3A6F66DD68C494885F2F3BD49AB407019BD0D894B8885DE7',
-       u'TokenType': u'iframe',
-       u'ValidFor': 500000}],
-     u'Total': 1}
-
-## TODOs:
-
-- tests
+## Combine an resource with an action
+``` python
+# Get the contact lists of contact #2
+result = mailjet.Contact_getcontactslists(id=2)
+```
--- mailjet/client.py
+++ mailjet/client.py
@@ -7,6 +7,7 @@ import logging
 import requests
 from requests.compat import urljoin
 
+requests.packages.urllib3.disable_warnings()
 
 class Config(object):
     API_URL = 'https://api.mailjet.com/v3/REST/'
@@ -92,31 +93,31 @@ class Config(object):
 
 class Endpoint(object):
 
-    def __init__(self, url, doc, auth):
-        self._url, self._doc, self._auth = url, doc, auth
+    def __init__(self, url, doc, auth, action=None):
+        self._url, self._doc, self._auth, self.action = url, doc, auth, action
 
     def __doc__(self):
         return self._doc
 
-    def _get(self, res_id=None, **kwargs):
-        return api_call(self._auth, 'get', self._url, resource_id=res_id, **kwargs)
+    def _get(self, filters=None, action_id=None, id=None, **kwargs):
+        return api_call(self._auth, 'get', self._url, action=self.action, action_id=action_id, filters=filters, resource_id=id, **kwargs)
 
-    def get_many(self, **kwargs):
-        return self._get(**kwargs)
+    def get_many(self, filters=None, action_id=None, **kwargs):
+        return self._get(filters=filters, **kwargs)
 
-    def get(self, res_id, **kwargs):
-        return self._get(res_id, **kwargs)
+    def get(self, id=None, filters=None, action_id=None, **kwargs):
+        return self._get(id=id, filters=filters, **kwargs)
 
-    def create(self, data, **kwargs):
-        return api_call(self._auth, 'post', self._url, data=data, **kwargs)
+    def create(self, data, filters=None, action_id=None, **kwargs):
+        return api_call(self._auth, 'post', self._url, data=data, action=self.action, action_id=action_id, filters=filters, **kwargs)
 
     new = create
 
-    def update(self, res_id, data, **kwargs):
-        return api_call(self._auth, 'put', self._url, resource_id=res_id, data=data, **kwargs)
+    def update(self, res_id, data, filters=None, action_id=None, **kwargs):
+        return api_call(self._auth, 'put', self._url, resource_id=res_id, data=data, action=self.action, action_id=action_id, filters=filters, **kwargs)
 
     def delete(self, res_id, **kwargs):
-        return api_call(self._auth, 'delete', self._url, resource_id=res_id, **kwargs)
+        return api_call(self._auth, 'delete', self._url, action=self.action, action_id=action_id, resource_id=res_id, **kwargs)
 
 
 class Client(object):
@@ -129,21 +130,25 @@ class Client(object):
         return self.endpoints
 
     def __getattr__(self, name):
+        split = name.split('_')
+        name = split[0]
+        if (len(split) > 1):
+            action = split[1]
         if name in self.endpoints:
             url, doc = self.config[name]
-            return type(name, (Endpoint,), {})(url=url, doc=doc, auth=self.auth)
+            return type(name, (Endpoint,), {})(url=url, doc=doc, action=action, auth=self.auth)
         raise AttributeError
 
 
-def api_call(auth, method, url, data=None, resource_id=None, extra_headers=None,
-             timeout=60, debug=False, **kwargs):
-    data, url = build_body_and_url(url, method=method,
-                                   data=data, resource_id=resource_id)
+def api_call(auth, method, url, data=None, filters=None, resource_id=None, extra_headers=None,
+             timeout=60, debug=False, action=None, action_id=None, **kwargs):
+    url = build_url(url, method=method, action=action, resource_id=resource_id, action_id=action_id)
+    print method, url, data
     headers = build_headers(extra_headers)
     req_method = getattr(requests, method)
 
     try:
-        response = req_method(url, data=data, headers=headers, auth=auth,
+        response = req_method(url, data=data, params=filters, headers=headers, auth=auth,
                               timeout=timeout, verify=False, stream=False)
 
         return parse_response(response, debug=debug)
@@ -167,13 +172,15 @@ def build_headers(extra_headers=None):
     return headers
 
 
-def build_body_and_url(url, method, data=None, resource_id=None):
-    data = json.dumps(data) if data else None
-
+def build_url(url, method, action=None, resource_id=None, action_id=None):
     if resource_id:
         url += '/%d' % resource_id
+    if action:
+        url += '/%s' % action
+        if action_id:
+            url += '/%d' % action_id
 
-    return data, url
+    return url
 
 
 def parse_response(response, debug=False):
@@ -188,29 +195,29 @@ def parse_response(response, debug=False):
         logging.debug('RESP_HEADERS: %s' % response.headers)
         logging.debug('RESP_CODE: %s' % response.status_code)
 
-    if response.status_code > 300:
-        errors = data
-        exc_data = dict(message=errors.get('ErrorMessage', None),
-                        info=errors.get('ErrorInfo', None),
-                        response_parsed=data,
-                        response=response,
-                        request=response.request,
-                        )
-
-    if response.status_code == 401:
-        raise AuthorizationError(exc_data)
-    elif response.status_code == 403:
-        raise ActionDeniedError(exc_data)
-    elif response.status_code == 404:
-        raise DoesNotExistError(exc_data)
-    elif response.status_code == 422:
-        raise ValidationError(exc_data)
-    elif response.status_code >= 500:
-        logging.error('Critical API error', exc_info=True, extra=exc_data)
-        raise CriticalApiError(errors)
-    elif response.status_code >= 400:
-        logging.error('API error', exc_info=True, extra=exc_data)
-        raise ApiError(exc_data)
+    # if response.status_code > 300:
+    #     errors = data
+    #     exc_data = dict(message=errors.get('ErrorMessage', None),
+    #                     info=errors.get('ErrorInfo', None),
+    #                     response_parsed=data,
+    #                     response=response,
+    #                     request=response.request,
+    #                     )
+    #
+    # if response.status_code == 401:
+    #     raise AuthorizationError(exc_data)
+    # elif response.status_code == 403:
+    #     raise ActionDeniedError(exc_data)
+    # elif response.status_code == 404:
+    #     raise DoesNotExistError(exc_data)
+    # elif response.status_code == 422:
+    #     raise ValidationError(exc_data)
+    # elif response.status_code >= 500:
+    #     logging.error('Critical API error', exc_info=True, extra=exc_data)
+    #     raise CriticalApiError(errors)
+    # elif response.status_code >= 400:
+    #     logging.error('API error', exc_info=True, extra=exc_data)
+    #     raise ApiError(exc_data)
 
     return data
 
