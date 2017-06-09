7596a82c73f4c699b84fc28ebfe55b7707ca5116
Arnaud BRETON <arn.breton@gmail.com>
2017-06-09 09:03:34 +0200

    Fixes #10: removing Python 3.2 in metadata

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
                  'Operating System :: OS Independent',
                  'Programming Language :: Python :: 2.6',
                  'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3.2',
                  'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
                  'Topic :: Utilities'],
