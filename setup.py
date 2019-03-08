cf1178ee56d220879cec0f89d4014764967e3a15
Todor Dimitrov <bucinator89@gmail.com>
2019-03-08 15:25:24 +0200

    Remove python 2.6 from supported versions, update request module to 2.21.0

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
                  'License :: OSI Approved :: GNU General Public License (GPL)',
                  'Natural Language :: English',
                  'Operating System :: OS Independent',
-                 'Programming Language :: Python :: 2.6',
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
